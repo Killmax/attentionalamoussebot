2924c8fd6d55b6c1acdf5336de94c39f2f4dc5b8
Maxime Bruguet <maxime.bruguet@gmail.com>
2019-12-03 22:04:28 +0100

    Global variable for bot and chat_id

--- mousse.py
+++ mousse.py
@@ -3,11 +3,12 @@ import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
+bot = telegram.Bot(token=os.getenv('bot_token'))
+chat_id = os.getenv('chat_id')
 
 def send_the_soap():
     bot.send_message(chat_id=chat_id, text="ATTENTION A LA MOUSSE ! /attentionalamousse")
 
 if __name__ == "__main__":
-    bot = telegram.Bot(token=os.getenv('bot_token'))
-    chat_id = os.getenv('chat_id')
+
     send_the_soap()
