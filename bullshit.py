4e763e0c29e36af3f2848f020ac7c679f744f5c4
Maxime Bruguet <maxime.bruguet@gmail.com>
2019-12-03 21:53:55 +0100

    A sentence for Raphi

--- bullshit.py
+++ bullshit.py
@@ -8,14 +8,13 @@ load_dotenv()
 bot = telegram.Bot(token=os.getenv("bot_token"))
 chat_id = os.getenv("chat_id")
 
-sentences = [
-    "Adrien prend du tofu à Chipotle. /adrofdp",
-    "KOGHONTA 🗝️",
-    "Bière ?"
-]
-
 def send_the_bullshit(bullshit):
     bot.send_message(chat_id=chat_id, text=bullshit)
 
 if __name__ == "__main__":
+    sentences = [
+        "/adrofdp",
+        "KOGHONTA 🗝️",
+        "TG Raphi @Raphicci"
+    ]
     send_the_bullshit(random.choice(sentences))
