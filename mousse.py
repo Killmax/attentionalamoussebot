8935ff249bdbe32ec78c2f4d5779d6ea4a9f07ab
Maxime Bruguet <maxime.bruguet@gmail.com>
2020-01-12 12:04:24 +0100

    Pimped mousse.py

--- mousse.py
+++ mousse.py
@@ -2,12 +2,15 @@ import telegram
 import time
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 bot = telegram.Bot(token=os.getenv('bot_token'))
 chat_id = os.getenv('chat_id')
+script_path = os.path.dirname(__file__)
 
 def send_the_soap():
-    bot.send_message(chat_id=chat_id, text="ATTENTION A LA MOUSSE ! /attentionalamousse")
+    mousse = open(os.path.join(script_path, "static/sound/mousse.mp3"), "rb")
+    bot.send_audio(chat_id=chat_id, audio=mousse, title="ATTENTION A LA MOUSSE !", caption="ATTENTION A LA MOUSSE ! /attentionalamousse")
 
 if __name__ == "__main__":
     send_the_soap()
