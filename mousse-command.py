9d1f7fa672fb2c7fc2a5f4fa59270b9c6521095d
Maxime Bruguet <maxime.bruguet@gmail.com>
2020-02-11 23:31:37 +0100

    V0.1

--- mousse-command.py
+++ mousse-command.py
@@ -6,14 +6,14 @@ import time
 import os
 import sqlite3
 from sqlite3 import Error
-from datetime import datetime, date
+from datetime import datetime, date, time
 from pytz import timezone
 from dotenv import load_dotenv
 
 load_dotenv()
-# chat_id = os.getenv('chat_id')
-chat_id = "68878627"
-bot_token=os.getenv('bot_token')
+chat_id = os.getenv('chat_id')
+bot_token = os.getenv('bot_token')
+admin_userid = os.getenv('admin_userid')
 script_path = os.path.dirname(__file__)
 
 # Enable logging
@@ -24,6 +24,9 @@ logger = logging.getLogger(__name__)
 scale = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
 
 class SoapState:
+    entries = []
+    is_open_for_race = False
+
     class Entry:
         def __init__(self, user_id, username, timestamp):
             self.username = username
@@ -107,41 +110,53 @@ class RankingDB:
 g_state = SoapState()
 g_db = RankingDB('mousseurs.db')
 
+def end_the_race(context):
+    todays_entries = g_state.get_entries()
+    rankings_string = "Here's the ranking of the day:\n"
+    for index in range(len(todays_entries)):
+        entry = todays_entries[index]
+        rankings_string += "P{index} : {username} @ {timestamp}\n".format(index=index + 1,username=entry.username, timestamp=entry.timestamp)
+        g_db.insert_competitor(str(entry.user_id), entry.username, scale[index])
+    context.bot.send_message(chat_id, text=rankings_string)
+    g_state.close_the_race()
+
 def send_the_soap(context):
     job = context.job
     mousse = open(os.path.join(script_path, "static/sound/mousse.mp3"), "rb")
     context.bot.send_audio(job.context, audio=mousse, title="ATTENTION A LA MOUSSE !", performer="Le mousseur fou", caption="ATTENTION A LA MOUSSE ! /attentionalamousse")
     g_state.open_the_race()
 
+def stop_the_soap(context):
+    end_the_race(context)
+
+
 def set_timer(update, context):
-    paris_tz = timezone('Europe/Paris')
-    today_date = date.today()
-    timer_datetime = datetime(today_date.year, today_date.month, today_date.day, 16, 0, 0, 0, tzinfo=paris_tz)
+    if update.effective_user.id != admin_userid: # FIX ME
+        update.message.reply_text('You cannot set the timer. Only an impressive programmer can do it.')
+    else:
+        paris_tz = timezone('Europe/Paris')
+        start_datetime = time(16, 0, 0, 0, tzinfo=paris_tz)
+        end_datetime = time(18, 0, 0, 0, tzinfo=paris_tz)
 
-    if 'job' in context.chat_data:
-        # Add a condition in order to be impossible to set the timer one hour before 4pm
-        update.message.reply_text('INFO : Removing the former timer')
-        old_job = context.chat_data['job']
-        old_job.schedule_removal()
+        if 'race_start' in context.chat_data:
+            update.message.reply_text('INFO : Removing the former timer')
+            old_job = context.chat_data['race_start']
+            old_job.schedule_removal()
 
-    new_job = context.job_queue.run_daily(send_the_soap, timer_datetime.replace(tzinfo=None), context=chat_id)
-    context.chat_data['job'] = new_job
-    
-    update.message.reply_text('Ready to send the soap.')
+        start_job = context.job_queue.run_daily(callback=send_the_soap, time=start_datetime.replace(tzinfo=None), context=chat_id)
+        context.chat_data['race_start'] = start_job
+        end_job = context.job_queue.run_daily(callback=stop_the_soap, time=end_datetime.replace(tzinfo=None), context=chat_id)
+        context.chat_data['race_end'] = end_job
+
+        update.message.reply_text('Ready to send the soap @ ' + start_datetime.isoformat() + '. End of the race : ' + end_datetime.isoformat())
 
 def mousse(update, context):
     if g_state.is_race_opened():
-        if g_state.get_number_entries() == len(scale): # Add two hours limit
-            todays_entries = g_state.get_entries()
-            rankings_string = "Here's the ranking of the day:\n"
-            for index in range(len(todays_entries)):
-                entry = todays_entries[index]
-                rankings_string += "P{index} : {username} @ {timestamp}\n".format(index=index + 1,username=entry.username, timestamp=entry.timestamp)
-                g_db.insert_competitor(str(entry.user_id), entry.username, scale[index])
-            context.bot.send_message(chat_id, text=rankings_string)
-            g_state.close_the_race()
+        if g_state.get_number_entries() == len(scale):
+            end_the_race(context)
         else:
-            timestamp = datetime.now().strftime('%H:%M:%S.%f')
+            # TODO : Needs absolutely to check if the user has already entered
+            timestamp = update.message.date.strftime('%H:%M:%S.%f')
             if (update.effective_user.username):
                 username = update.effective_user.username
             else:
@@ -158,15 +173,12 @@ def help(update, context):
     update.message.reply_text('Rules : Send the command /attentionalamousse once the bot has sent the message.\n' + 
                             'You have 2 hours to send the command as fast as possible\n' + 
                             'P1 25pts, P2 18pts, P3 15pts, P4 12pts, P5 10pts, P6 8pts, P7 6pts, P8 4pts, P9 2pts, P10 1pt]\n' + 
-                            'You can also send the command /rankings to see the standings.\n' +
-                            'Don\' forget to init me by sending me the command /setthesoap')
+                            'You can also send the command /rankings to see the standings.)
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def main():
-    # To be removed, only for test purposes
-    g_state.open_the_race()
 
     sqlite_db_connection = g_db.get_connection()
 
