e74dcb47053c8eeecc61d7aee8a0482877a2b317
Maxime Bruguet <maxime.bruguet@gmail.com>
2020-03-06 18:40:31 +0100

    Added rankings and refactor

--- soapclass/RankingDB.py
+++ soapclass/RankingDB.py
@@ -1,5 +1,4 @@
 import sqlite3
-from sqlite3 import Error
 from datetime import datetime
 
 scale = [25, 18, 15, 12, 10, 8, 6, 4, 2, 1]
@@ -9,14 +8,14 @@ class RankingDB:
         self.connection = None
         try:
             self.connection = sqlite3.connect(filename, check_same_thread=False)
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
 
     def create_table(self, request):
         try:
             cursor = self.connection.cursor()
             cursor.execute(request)
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
     
     def get_connection(self):
@@ -42,8 +41,34 @@ class RankingDB:
                 new_competitor_data = (user_id, username, points_to_add, datetime.now().strftime('%d/%m/%Y') if points_to_add == scale[0] else None)
                 cursor.execute(creation_request, new_competitor_data)
             self.connection.commit()
-        except Error as e:
+        except sqlite3.Error as e:
             print(e)
+
+    def get_rankings(self):
+        ranking_requests = "SELECT * FROM mousseurs ORDER BY points DESC"
+        ranking_string = ""
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(ranking_requests)
+            rows = cursor.fetchall()
+            index = 1
+            if (len(rows) == 0):
+                return "No races yet."
+            for row in rows:
+                username = row[1]
+                points = row[2]
+                last_win = row[3]
+                ranking_string += "P{index} : {username} - {points} pt(s) ".format(index=index,username=username, points=points)
+                index += 1
+                if (last_win):
+                    ranking_string += "- Last win : {last_win}\n".format(last_win=last_win)
+                else:
+                    ranking_string += "- No win\n"
+            cursor.close()
+        except sqlite3.Error as e:
+            print(e)
+        
+        return ranking_string
 
     def __init__(self, filename):
         sql_create_mousseurs_table = """ CREATE TABLE IF NOT EXISTS mousseurs (
